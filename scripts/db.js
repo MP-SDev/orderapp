3ff74673d24169f78b734a64b4fdadebaf881bb6
Marcel Pootz-Sieker <mpsdev@posteo.de>
2026-09-14 07:12:34 +0200

    add db.js and create dishes object with all meals, pictures and informations

---
+++ scripts/db.js
@@ -0,0 +1,98 @@
+let dishes = [
+    {
+        "name": "Veggie mushroom black burger",
+        "desc": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+        "price": 16.90,
+        "img": url('./assets/img/veggie-mushroom-black-burger.jpg'),
+        "category": "burger",
+        "amount": 0
+    },
+    {
+        "name": "All meat burger",
+        "desc": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+        "price": 15.90,
+        "img": url('./assets/img/all-meat-burger.jpg'),
+        "category": "burger",
+        "amount": 0
+    },
+    {
+        "name": "Beef red burger",
+        "desc": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+        "price": 14.90,
+        "img": url('./assets/img/beef-red-burger.jpg'),
+        "category": "burger",
+        "amount": 0
+    },
+    {
+        "name": "Big chicken burger",
+        "desc": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+        "price": 15.90,
+        "img": url('./assets/img/big-chicken-burger.jpg'),
+        "category": "burger",
+        "amount": 0
+    },
+    {
+        "name": "Pizza Margherita",
+        "desc": "Tomato Sauce, Mozzarella",
+        "price": 11.90,
+        "img": url('./assets/img/pizza-margherita.jpg'),
+        "category": "pizza",
+        "amount": 0
+    },
+    {
+        "name": "Pizza Chorizo",
+        "desc": "Tomato slices, Mozzarella, Chorizo",
+        "price": 13.90,
+        "img": url('./assets/img/pizza-chorizo.jpg'),
+        "category": "pizza",
+        "amount": 0
+    },
+    {
+        "name": "Funghi",
+        "desc": "Red onion, Olives, Button Mushrooms, Mozzarella",
+        "price": 12.90,
+        "img": url('./assets/img/pizza-funghi.jpg'),
+        "category": "pizza",
+        "amount": 0
+    },
+    {
+        "name": "Quattro Formaggi with Chicken",
+        "desc": "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+        "price": 15.90,
+        "img": url('./assets/img/pizza-quattro-formaggi.jpg'),
+        "category": "pizza",
+        "amount": 0
+    },
+    {
+        "name": "Warm beef arugula salad",
+        "desc": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+        "price": 16.90,
+        "img": url('./assets/img/arugula-salad.jpg'),
+        "category": "salad",
+        "amount": 0
+    },
+    {
+        "name": "Mini green Salad",
+        "desc": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+        "price": 7.90,
+        "img": url('./assets/img/mini-green-salad.jpg'),
+        "category": "salad",
+        "amount": 0
+    },
+    {
+        "name": "Green Salad with sea food",
+        "desc": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+        "price": 16.90,
+        "img": url('./assets/img/green-salad-seafood.jpg'),
+        "category": "salad",
+        "amount": 0
+    },
+    {
+        "name": "Vegan green salad with tofu",
+        "desc": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+        "price": 14.90,
+        "img": url('./assets/img/green-salad-tofu.jpg'),
+        "category": "salad",
+        "amount": 0
+    }
+]
